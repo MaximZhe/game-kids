ee45b813491fdf50fa6c2b14f5a50a0588fc0ed2
Maxim <Sen989@yandex.ru>
2022-07-22 21:18:55 +0700

    Day4

--- js/main.js
+++ js/main.js
@@ -11,7 +11,7 @@ const imgRandom = [
 "img/2.png",
 "img/3.png",
 "img/4.png",
-"img/5.png"].sort(() => Math.random() - 0.5)
+"img/5.png"].sort(() => Math.random() - 0.5);
 
 let imgsTop = new Set([...imgRandom]);
 let imgsReverse = imgRandom.reverse();
@@ -28,38 +28,82 @@ sumImgs.forEach((item) => {
     img.setAttribute("data-num",item.match(/\d/));
     container.append(card);
     card.append(img);
-})
+});
 
 let hasFlippedCard = false;
-let i;
-let r;
-
+let lockCard = 2;
+let oneCard;
+let twoCard;
+let f = [];
+let activeCard = document.querySelectorAll(".card");
+let count = 0;
 function flipCard(e) {
+    
     if(e.target.classList.contains("noactive")){
         e.target.classList.add("active");
     }
-
+    
     if (!hasFlippedCard) {
         hasFlippedCard = true;
-        i = e.target.childNodes[0].dataset.num;
+        oneCard = e.target;
+        f.push(oneCard);
         return;
-    }else{
-        r = e.target.childNodes[0].dataset.num;;
-        hasFlippedCard = false;
+
     }
-   console.log(i,rgit)
+    if(hasFlippedCard){
+        twoCard = e.target;
+        hasFlippedCard = false;
+        f.push(twoCard);
+    }
+    if(f.length === 2){
+        activeCard.forEach(i => {
+            i.classList.add("no-click");
+        });
+    }
     
-
-   checkForMatch();
+    if (oneCard.childNodes[0].dataset.num === twoCard.childNodes[0].dataset.num) {
+    oneCard.classList.add("succeed");
+    twoCard.classList.add("succeed");
+    count += 2;
+    setTimeout(() => {
+        activeCard.forEach(i => {
+            i.classList.remove("no-click");  
+        });
+    },2000);
+    
+    }else{
+        activeCard.forEach(i => {
+            setTimeout(() => {
+            i.classList.remove("active", "no-click");
+            },2000);
+            
+        });
+        
+    }
+    f = [];
+    console.log(count);
+    if(count === activeCard.length){
+        finishGame();
+    }
  }
-
- function checkForMatch() {
- if (i.dataset.num === r.dataset.num) {
-     return;
-   }
-
-
+ 
+ function finishGame() {
+   const finishMassege = document.createElement("div");
+    finishMassege.classList.add("massege");
+    finishMassege.innerHTML = "Ты победил!!!";
+    container.append(finishMassege);
+    finishMassege.addEventListener("click", () => {
+   
+        while (container.firstChild) {
+                container.removeChild(container.firstChild);
+            }
+        });
  }
+ 
+
 
 container.addEventListener("click", flipCard);
+
+
+
 
